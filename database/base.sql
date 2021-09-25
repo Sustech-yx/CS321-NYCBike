a6e3797ae1918ccc6ed314a99b8009881d046d5c
Sustech-yx <59305237+Sustech-yx@users.noreply.github.com>
2021-09-25 21:08:35 +0800

    写了一些创建关系数据库表的sql语句，下一步是写一些基本的查询语句（例如：查询具体的车站的流量情况，查询某一辆单车的行为情况等）。

--- database/base.sql
+++ database/base.sql
@@ -1,6 +1,13 @@
+CREATE TABLE IF NOT EXISTS 'Region_table'(
+    'region_id' INT INCREMENT,
+    'region_name' VARCHAR(100),
+    PRIMARY KEY ('region_id')
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE IF NOT EXISTS 'Station_table'(
     'station_id' VARCHAR(100) NOT NULL,
     'station_name' VARCHAR(100) NOT NULL,
+    'region_id' VARCHAR(100) NOT NULL,
     'Lat_Deg' NUMBER NOT NULL,
     'Lat_Min' NUMBER NOT NULL,
     'Lat_Sec' NUMBER NOT NULL,
@@ -11,6 +18,15 @@ CREATE TABLE IF NOT EXISTS 'Station_table'(
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS 'Order_table'(
-    'order_id' VARCHAR(100) NOT NULL,
-
+    'order_id' INT INCREMENT,
+    'start_station_id' VARCHAR(100) NOT NULL,
+    'end_station_id' VARCHAR(100) NOT NULL,
+    'start_time' DATETIME_INTERVAL_CODE NOT NULL,
+    'end_time' DATETIME_INTERVAL_CODE NOT NULL,
+    'trip_duration' INT NOT NULL,
+    'bike_id' VARCHAR(100) NOT NULL,
+    'gender' SMALLINT NOT NULL,
+    'usertype' VARCHAR(10) NOT NULL,
+    'birth_year' INT NOT NULL,
+    PRIMARY KEY ('order_id')
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
